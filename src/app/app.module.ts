99452301c5b6071a1b2c1e52bcb2bfd4ad6d016f
Edwin <mredwinrodriguez@gmail.com>
2018-12-13 22:01:02 -0500

    navbar and create user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent,
+    CreateAccountComponent
   ],
   imports: [
     BrowserModule
