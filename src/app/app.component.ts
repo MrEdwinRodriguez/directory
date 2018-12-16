10b48893e02ae63e4c40600a5ba068a618144c25
Edwin <mredwinrodriguez@gmail.com>
2018-12-16 15:37:35 -0500

    added login, create account css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directory-app';
+  loadedFeature = 'login';
+
+  onNavigate(feature: string) { 
+    this.loadedFeature = feature
+  }
 }
