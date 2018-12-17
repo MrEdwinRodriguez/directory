e775f01b96640665e9217527b1c93ae66e3df911
Edwin <mredwinrodriguez@gmail.com>
2018-12-16 21:15:59 -0500

    profile front end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  loadedFeature = 'login';
+  loadedFeature = '1login';
 
   onNavigate(feature: string) { 
     this.loadedFeature = feature
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LoginComponent,
-    CreateAccountComponent
+    CreateAccountComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
