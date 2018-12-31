edb82f7ce9bd9086e0924ac5cbf01d294f97f3ef
Edwin <mredwinrodriguez@gmail.com>
2018-12-31 15:49:09 -0500

    added ngmodel to create

--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Profile } from '../profile/profile.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-create-account',
@@ -6,6 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-account.component.css']
 })
 export class CreateAccountComponent implements OnInit {
+
+
+  onCreateProfile(form: NgForm){
+
+    const profile: Profile ={
+      name: form.value.title,
+      email: form.value.email,
+      telephone: form.value.telephone,
+      occupation: form.value.occupation,
+      industry: form.value.industry,
+      location: form.value.location,
+      orginization: form.value.orginization,
+      chapter: form.value.chapter,
+      whoiknow: form.value.whoiknow,
+      businessname: form.value.businessname,
+      businesswebsite: form.value.businesswebsite,
+      businessdescription: form.value.businessdescription,
+      info: form.value.info
+
+    }
+
+  }
 
   constructor() { }
 
